0f2df838649256f5fc44449c8d1d9556f992b78b
janvi <janvisusan@gmail.com>
2021-01-03 21:02:58 -0800

    bank application project

--- bank.js
+++ bank.js
@@ -86,19 +86,33 @@ static withdraw=()=>
     let user = document.querySelector("#user").value
     let pwd = document.querySelector("#pass").value
     let amot=Number( document.querySelector("#amt").value)
-    let dataset=Bank.getData()
-    if (user in dataset) {
-        if (dataset[user].password == pwd) {
-            dataset[user].balance-=amot
-            alert("your accont is debited with"+amot+ "and final balance is"+dataset[user].balance)
-        }
-        else {
-            alert("incorrect password");
-        }
+    let us=Bank.authenticateUser(user,pwd)
+    let dataset = Bank.getData()
+    if(us==1){
+        dataset[user].balance-=amot
+        alert("your accont is credited with"+amot+ "and final balance is"+dataset[user].balance)
     }
-    else {
+    else if(us==0){
+        alert("incorrect password");
+    }
+    else if(us==-1){
         alert("No user exist");
-    }
+    }  // let user = document.querySelector("#user").value
+    // let pwd = document.querySelector("#pass").value
+    // let amot=Number( document.querySelector("#amt").value)
+    // let dataset=Bank.getData()
+    // if (user in dataset) {
+    //     if (dataset[user].password == pwd) {
+    //         dataset[user].balance-=amot
+    //         alert("your accont is debited with"+amot+ "and final balance is"+dataset[user].balance)
+    //     }
+    //     else {
+    //         alert("incorrect password");
+    //     }
+    // }
+    // else {
+    //     alert("No user exist");
+    // }
 }
 }
 
